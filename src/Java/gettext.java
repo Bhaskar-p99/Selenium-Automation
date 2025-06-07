ff0f5376ae50fc60dbd4c3504d026c419d45362b
Bhaskar_Pratigar <74720531+Bhaskar-p99@users.noreply.github.com>
2025-06-07 22:56:59 +0530

    Update gettext.java

--- src/Java/gettext.java
+++ src/Java/gettext.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//Using all get methods
 public class gettext {
 
 	public static void main(String[] args) {
