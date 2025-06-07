7127878fc531755645e0668e77773a0aa7b15f81
Bhaskar_Pratigar <74720531+Bhaskar-p99@users.noreply.github.com>
2025-06-07 22:51:37 +0530

    Update frames1.java

--- src/Java/frames1.java
+++ src/Java/frames1.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//Switch Frames then click on element
 public class frames1 {
 
 	public static void main(String[] args) {
