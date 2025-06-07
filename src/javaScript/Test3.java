751541fbcee4bf82768df7dfccdef163885a802e
Bhaskar_Pratigar <74720531+Bhaskar-p99@users.noreply.github.com>
2025-06-07 23:24:48 +0530

    Update Test3.java

--- src/javaScript/Test3.java
+++ src/javaScript/Test3.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//Mark red coclour on Login Button
 public class Test3 {
 
 	public static void main(String[] args) {
