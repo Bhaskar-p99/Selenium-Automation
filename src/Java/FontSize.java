5dee181fe767fe9d218d900589a92670660404d6
Bhaskar_Pratigar <74720531+Bhaskar-p99@users.noreply.github.com>
2025-06-07 22:41:17 +0530

    Update FontSize.java

--- src/Java/FontSize.java
+++ src/Java/FontSize.java
@@ -3,7 +3,7 @@ package Java;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//get font size
 public class FontSize {
 	public static void main(String[]args) {
 		
