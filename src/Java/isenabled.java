ac647f410e454e2cea2a8169bf229091d80c19b3
Bhaskar_Pratigar <74720531+Bhaskar-p99@users.noreply.github.com>
2025-06-07 22:57:46 +0530

    Update isenabled.java

--- src/Java/isenabled.java
+++ src/Java/isenabled.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//check Web element is Enabled
 public class isenabled {
 
 	public static void main(String[] args) throws InterruptedException {
